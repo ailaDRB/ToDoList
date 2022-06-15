94fe80aba8426b856095e7906617577c6a46214f
Dalia <104073174+ailaDRB@users.noreply.github.com>
2022-06-15 11:45:18 +0100

    Add files via upload

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,68 @@
+import { ITask } from '../navbar/model/task';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import{FormGroup,FormBuilder,Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+todoForm !: FormGroup;
+tasks : ITask [] = [];
+inprogress : ITask[] = [];
+done: ITask[] = [];
+updateIndex! :any;
+isEditEnable : Boolean = false;
+  constructor(private fb : FormBuilder) { }
+
+  ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item : ['',Validators.required]
+    })
+  }
+  addTask(){
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done:false,
+    })
+    this.todoForm.reset();
+  }
+  onEdit(item:ITask, i : number){
+   this.todoForm.controls['item'].setValue(item.description)
+   this.updateIndex= i;
+   this.isEditEnable = true;
+  }
+  updateTask(){
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.isEditEnable = false;
+  }
+  deleteTask(i: number){
+    this.tasks.splice(i,1)
+       
+  }
+  deleteInProgressTask(i: number){
+    this.inprogress.splice(i,1)
+       
+  }
+  deleteDoneTask(i: number){
+    this.done.splice(i,1)
+       
+  }
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
